986ca355bf3477e0380089dc78928ee4e2646406
DanielBradleyBennett <danielbradleybennett@gmail.com>
2020-01-30 14:45:44 -0600

    news component coded add

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,3 +1,30 @@
+import React from "react"
+
+
+
+export default ({ news }) => (
+    <section className="news_List">
+        <div className="news__title">{news.title}</div>
+        <div className="news__synopsis">{news.synopsis}</div>
+        <div className="news__url">{news.url}</div>
+
+
+    </section>
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // build the function creates the jsx for an event card
 // have an edit and delete button
 // display the event, name, date, who posted, and url
--- src/components/news/NewsList.js
+++ src/components/news/NewsList.js
@@ -1,36 +1,51 @@
 import React, { useContext } from "react"
 import { NewsContext } from "./NewsProvider"
-import {FriendsContext} from "../user/FriendsProvider"
+import { FriendsContext } from "../user/FriendsProvider"
 import News from "./News"
 // import "./Animals.css"
 
 
 export default (props) => {
   const { news } = useContext(NewsContext)
-  const {friends} = useContext(FriendsContext)
+  const { friends } = useContext(FriendsContext)
   const currentUser = parseInt(localStorage.getItem("currentUserId"), 10)
   const currentUserFriends = friends.filter(friend => friend.userId === currentUser)
-    const currentUserFriendsNews = 
-        currentUserFriends.map(friend => {
-            const currentFriendsNews = news.filter(n => n.userId === friend.id)
-            return currentFriendsNews
-  
+  const currentUserFriendsNews =
+    currentUserFriends.map(friend => {
+      const currentFriendsNews = news.filter(n => n.userId === friend.id)
+      return currentFriendsNews
 
-  })
-  
+
+    })
+    const currentUserNews = news.filter(news => news.userId === currentUser)
+    
   return (
     <div className="news">
       <h1>News</h1>
-      <button onClick={ 
+      <button onClick={
         () => props.history.push("/news/create")}>
         Add News
           </button>
       <article className="newsList">
 
 
+
         {
           currentUserFriendsNews.map(news => {
-            return <News key={news.id} news={news}/>
+            return <News key={news.id} news={news} />
+
+
+          })
+
+        }
+      </article>
+      <article className="newsList">
+
+
+
+        {
+          currentUserNews.map(news => {
+            return <News key={news.id} news={news} />
 
 
           })
@@ -40,6 +55,8 @@ export default (props) => {
     </div>
   )
 }
+
+
 
 
 
