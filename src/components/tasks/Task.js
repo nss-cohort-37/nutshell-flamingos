3581217fdade99aa8c83ba93d77b4953cbe87676
jwgilliam <ggsmoustache@gmail.com>
2020-01-30 14:04:46 -0600

    added task component

--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -1,3 +1,18 @@
+import React from "react"
+import "./Tasks.css"
+
+export default ({ task }) => (
+    <section className="task">
+        <h3 className="task__name">
+        { task.text }
+        </h3>
+        <button className="complete__task">Complete</button>
+    </section>
+)
+
+
+
+
 // build the function creates the jsx for an event card
 // have an edit and delete button
 // display the event, name, date, who posted, and url
