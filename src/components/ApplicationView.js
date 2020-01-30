77eec77aefe886a6c06fba6bb57e8ac17f50e5e4
macgibbons <macgibbons@gmail.com>
2020-01-30 09:27:21 -0600

    user login functionality now working

--- src/components/ApplicationView.js
+++ src/components/ApplicationView.js
@@ -1,1 +1,18 @@
+import React from "react"
+import { Route } from "react-router-dom"
+
+
 // renders the different page views utilising route
+export default (props) => {
+    return (
+        <>
+            
+            
+        </>
+    )
+}
+
+// You need to use the history.push() method again to take the user to a new view. Reference the code above to see how to do the following two tasks.
+
+// Provide the React Router history object to the EmployeeForm component by refactoring ApplicationViews.
+// Route the user to /employees.
