b7c0d59a83cc89cf0761844c8d10caafdf3e2da6
jwgilliam <ggsmoustache@gmail.com>
2020-01-30 13:15:31 -0600

    working on task list

--- src/components/tasks/TasksList.js
+++ src/components/tasks/TasksList.js
@@ -1,3 +1,45 @@
+import React, { useContext } from "react"
+import { TasksContext } from "./TasksProvider"
+import Task from "./Task"
+import "./Tasks.css"
+
+
+
+
+export default (props) => {
+  const { tasks } = useContext(TasksContext)
+  const  currentUser  = parseInt(localStorage.getItem("currentUserId"), 10)
+  
+
+  return (
+      <>
+          <h1>Tasks</h1>
+
+          <button onClick={() => props.history.push("/tasks/create")}>
+            Save Task
+          </button>
+          <div className="tasks">
+              {
+                  tasks.map(task => {
+                      return <Task key={task.id} task={task} />
+                  })
+              }
+          </div>
+      </>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // have a link to route user to event form
 // filter over array of friends and invoke .js function before every event whos userId matches current userId
 // 
