45d827da05812a122f3d59a4efa2d148ad63dfd5
macgibbons <macgibbons@gmail.com>
2020-01-30 10:07:56 -0600

    created the News provider

--- src/components/news/NewsProvider.js
+++ src/components/news/NewsProvider.js
@@ -1,3 +1,53 @@
-// export event context
-// export get add delete edit
-// 
+import React, { useState, useEffect } from "react"
+
+/*
+    The context is imported and used by individual components
+    that need data
+*/
+export const NewsContext = React.createContext()
+
+/*
+ This component establishes what data can be used.
+ */
+export const NewsProvider = (props) => {
+    const [news, setNews] = useState([])
+
+    const getNews = () => {
+        return fetch("http://localhost:8088/newss")
+            .then(res => res.json())
+            .then(setNews)
+    }
+
+    const addNews = News => {
+        return fetch("http://localhost:8088/news", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(news)
+        })
+            .then(getNews)
+    }
+
+    /*
+        Load all animals when the component is mounted. Ensure that
+        an empty array is the second argument to avoid infinite loop.
+    */
+    useEffect(() => {
+        getNewss()
+    }, [])
+
+    useEffect(() => {
+        console.log("****  News APPLICATION STATE CHANGED  ****")
+        
+    }, [news])
+
+    return (
+        <NewsContext.Provider value={{
+            news, addNews
+        }}>
+            {props.children}
+        </NewsContext.Provider>
+    )
+}
+
