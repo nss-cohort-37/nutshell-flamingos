705c2939fcc951f5d65f95d01587d5abdeec7451
wildwes <weswaters@rocketmail.com>
2020-01-31 09:43:17 -0600

    built message provider

--- src/components/messages/MessagesProvider.js
+++ src/components/messages/MessagesProvider.js
@@ -1,37 +1,72 @@
-// export event context
-// export get add delete edit
-// 
-let messages = [];
+import React, { useState, useEffect } from "react"
 
-export const useMessages = () => messages.slice();
+/*
+The context is imported and used by individual components
+that need data
+*/
 
-export const getMessages = () =>
-  fetch("http://localhost:8088/messages?_expand=user")
+export const MessagesContext = React.createContext()
+
+/*
+This component establishes what data can be used.
+*/
+export const MessagesProvider = (props) => {
+  const [messages, setMessages] = useState([])
+  
+  const getMessages = () => {
+    return fetch("http://localhost:8088/messages")
     .then(res => res.json())
-    .then(parsedMessages => messages = parsedMessages);
+    .then(setMessages)
+  }
+  
+  const addMessages = messages => {
+    return fetch("http://localhost:8088/messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(messages)
+    })
+    .then(getMessages)
+  }
+  
+  const editMessages = messages => {
+    return fetch(`http://localhost:8088/messages/${messages.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(messages)
+    })
+        .then(getMessages)
+}
 
-export const editMessages = messagesObject => {
-  return fetch(`http://localhost:8088/messages/${messagesObject.id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(messagesObject)
-  }).then(getMessages);
-};
+const deleteMessages = messagesId => {
+    return fetch(`http://localhost:8088/messages/${messagesId}`, {
+        method: "DELETE"
+    })
+        .then(getMessages)
+}
 
-export const deleteMessages = messagesId => {
-  return fetch(`http://localhost:8088/messages/${messagesId}`, {
-    method: "DELETE"
-  }).then(getMessages);
-};
+ useEffect(() => {
+   getMessages()
+  }, [])
+  
+  // useEffect(() => {
+    //     console.log("****  Messages APPLICATION STATE CHANGED  ****")
+    
+    // }, [messages])
+    
+    return (
+      <MessagesContext.Provider value={{
+        messages, addMessages, editMessages, deleteMessages
+      }}>
+            {props.children}
+        </MessagesContext.Provider>
+    )
+  }
 
-export const saveMessages = messages => {
-  return fetch("http://localhost:8088/messages", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(messages)
-  }).then(getMessages);
-};
+
+
+  // export message context
+  // export get add delete edit
